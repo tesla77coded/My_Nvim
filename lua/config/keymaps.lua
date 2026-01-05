454ebcb8d9a1fed2cb4fc0c6984ddec0888e63f2
tesla77coded <kadamjaydeep60@gmail.com>
2026-01-05 14:03:17 +0530

    Integrated new keymaps for trouble.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,4 @@
 -- lua/config/keymaps.lua
-
 local map = vim.keymap.set
 local opts = function(desc)
 	return { noremap = true, silent = true, desc = desc }
@@ -10,41 +9,34 @@ end
 ----------------------------------------------------------------
 -- Esc alternative
 map("i", "jk", "<Esc>", opts("Exit Insert"))
-
 --  Window focus cycle (jump between splits/explorer/explorer/notifications)
 map("n", "<leader>w", "<C-w>w", opts("Next Window"))
-
 -- Save file
 map("n", "<leader>s", "<cmd>w<cr>", opts("Save File"))
-
 -- Buffers
 map("n", "<S-h>", "<cmd>bprevious<cr>", opts("Prev Buffer"))
 map("n", "<S-l>", "<cmd>bnext<cr>", opts("Next Buffer"))
 map("n", "<leader>bd", "<cmd>bdelete<cr>", opts("Delete Buffer"))
-
 -- Clear search highlights
 map("n", "<esc>", "<cmd>nohlsearch<cr>", opts("Clear Search"))
-
 -- Lazy manager
 map("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Open Lazy plugin manager" })
-
 -- Normal mode: move current line
 map("n", "<A-j>", "<cmd>move .+1<cr>==", opts("Move Line Down"))
 map("n", "<A-k>", "<cmd>move .-2<cr>==", opts("Move Line Up"))
-
 -- Insert mode: move current line and stay in insert
 map("i", "<A-j>", "<Esc><cmd>move .+1<cr>==gi", opts("Move Line Down"))
 map("i", "<A-k>", "<Esc><cmd>move .-2<cr>==gi", opts("Move Line Up"))
-
 -- Visual mode: move selected lines
 map("v", "<A-j>", ":move '>+1<cr>gv=gv", opts("Move Lines Down"))
 map("v", "<A-k>", ":move '<-2<cr>gv=gv", opts("Move Lines Up"))
-
 -- Yank line without new line character
 map("n", "yy", "^y$")
+
 ----------------------------------------------------------------
 -- LSP
 ----------------------------------------------------------------
+
 map("n", "K", vim.lsp.buf.hover, opts("Hover"))
 map("n", "gd", vim.lsp.buf.definition, opts("Goto Definition"))
 map("n", "gD", vim.lsp.buf.declaration, opts("Goto Declaration"))
@@ -56,11 +48,21 @@ map("n", "<leader>cf", function()
 	vim.lsp.buf.format({ async = true })
 end, opts("Format"))
 
--- Diagnostics (Neovim 0.11+)
-map("n", "<leader>cd", vim.diagnostic.open_float, opts("Line Diagnostics"))
+-- Diagnostics (now using Trouble)
+map("n", "<leader>cd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", opts("Buffer Diagnostics (Trouble)"))
 map("n", "]d", vim.diagnostic.goto_next, opts("Next Diagnostic"))
 map("n", "[d", vim.diagnostic.goto_prev, opts("Prev Diagnostic"))
-map("n", "<leader>cl", vim.diagnostic.setloclist, opts("Diagnostics → Loclist"))
+map("n", "<leader>cl", "<cmd>Trouble diagnostics toggle<cr>", opts("All Diagnostics (Trouble)"))
+
+----------------------------------------------------------------
+-- Trouble.nvim
+----------------------------------------------------------------
+map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", opts("Diagnostics (Trouble)"))
+map("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", opts("Buffer Diagnostics (Trouble)"))
+map("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", opts("Symbols (Trouble)"))
+map("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", opts("Location List (Trouble)"))
+map("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", opts("Quickfix List (Trouble)"))
+map("n", "<leader>xr", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", opts("LSP References (Trouble)"))
 
 ----------------------------------------------------------------
 -- Snacks (picker + explorer)
@@ -73,7 +75,6 @@ if ok_snacks then
 end
 
 -- Open snacks explorer at current file
-
 vim.keymap.set("n", "<leader>e", function()
 	require("snacks").explorer({ cwd = vim.fn.expand("%:p:h") })
 end, { desc = "Explorer at current file" })
@@ -107,5 +108,6 @@ if ok_wk then
 		{ "<leader>f", group = "Find" },
 		{ "<leader>g", group = "Git" },
 		{ "<leader>b", group = "Buffers" },
+		{ "<leader>x", group = "Trouble/Diagnostics" },
 	})
 end
